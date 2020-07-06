8db68f69fe23e51fe1eceda11f8280a5771e6050
Smephey <smephey@gmail.com>
2020-07-06 22:01:41 +0100

    Grid now leaves an orange trail where the mouse hovered over

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const containerDiv = document.createElement('div');
+containerDiv.className = 'container';
 
-for(let row = 0; row <= 15; row++) {
-    const row = document.createElement('div');
-    row.setAttribute('style', 'width: 100%; display: flex');
-    for(let column = 0; column <= 15; column++) {
-        const div = document.createElement('div');
-        div.setAttribute("style", "border: 1px solid black; padding: 25px 25px");
-        row.appendChild(div);
+
+function drawGrid() {
+    for(let row = 0; row <= 15; row++) {
+        const row = document.createElement('div');
+        row.setAttribute('style', 'width: 100%; display: flex');
+        for(let column = 0; column <= 15; column++) {
+            const div = document.createElement('div');
+            div.setAttribute("style", "border: 1px solid black; padding: 25px 25px");
+            setHover(div);
+            row.appendChild(div);
+        }
+        containerDiv.appendChild(row);
     }
-    containerDiv.appendChild(row);
 }
 
+function setHover(square) {
+    square.addEventListener('mouseover', (square) => {
+        square.target.style.backgroundColor = 'orange'
+    })
+}
 
-containerDiv.className = 'container';
+drawGrid();
 
 document.body.appendChild(containerDiv);
