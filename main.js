47c06029016ac5a8754d28ebae396caf3401735a
Smephey <smephey@gmail.com>
2020-07-06 21:46:54 +0100

    Grid in place

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const containerDiv = document.createElement('div');
 
 for(let row = 0; row <= 15; row++) {
+    const row = document.createElement('div');
+    row.setAttribute('style', 'width: 100%; display: flex');
     for(let column = 0; column <= 15; column++) {
         const div = document.createElement('div');
-        div.setAttribute("style", "border: 1px solid black; padding: 1px 1px");
-        containerDiv.appendChild(div);
+        div.setAttribute("style", "border: 1px solid black; padding: 25px 25px");
+        row.appendChild(div);
     }
+    containerDiv.appendChild(row);
 }
 
 
